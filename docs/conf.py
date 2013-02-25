d8ac5d55f380eef26af1a05264d969367dc01fa9
wohugb <wohugb@me.com>
2013-02-25 22:13:52 +0800

    cn->zh

--- docs/conf.py
+++ docs/conf.py
@@ -18,6 +18,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 project = u'Read The Docs'
 copyright = u'2010, Eric Holscher, Charlie Leifer, Bobby Grace'
+language = 'zh_CN'
 version = '1.0'
 release = '1.0'
 exclude_patterns = ['_build']
